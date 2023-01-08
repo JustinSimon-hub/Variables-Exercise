1750ced47a84f067a0254acea0473503b1f72836
Justin Simon <justinsimon@Justins-MBP.lan>
2023-01-08 15:38:25 -0800

    Finished the assignment

--- VariablesExercise/Program.cs
+++ VariablesExercise/Program.cs
@@ -3,8 +3,15 @@
     public class Program
     {
         static void Main(string[] args)
+
         {
-            Console.WriteLine("Hello, World!");
+            string name = "Justin is my name" ;
+            int age = 14;
+            char alphabet = 's';
+            bool trueorfalseStatement = true;
+            double money = 129.9;
+            decimal tempature = 101.2m;
+            Console.WriteLine($"my name is {name}, I was {age} 7 years ago, my favorite letter in the alphabet is {alphabet}. I always try and tell the {trueorfalseStatement} no matter what. I used to have {money} in my account now its much more. Yesterday I knew I had a fever when my tempature was {tempature}");
         }
     }
 }
